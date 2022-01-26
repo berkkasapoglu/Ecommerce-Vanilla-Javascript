2ceff56e94bf052d543515b1897f533f2b16f9f0
Berk Kasapoglu <berkkasapoglu1@gmail.com>
2022-01-26 22:37:55 +0300

    Pagination added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const engine = require('ejs-mate');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const { collection } = require('./models/product');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/Ecommerce')
@@ -18,7 +19,7 @@ app.engine('ejs', engine);
 
 app.get('/', async (req, res) => {
     const products = await Product.find({}).limit(10);
-    const categories = await Product.distinct('category',)
+    const categories = await Product.distinct('category')
     res.render('index.ejs', { products, categories });
 })
 
@@ -28,9 +29,9 @@ app.get('/products/:id', async (req, res) => {
     res.render('product.ejs', { product });
 })
 
-app.get('/c/', async (req, res) => {
-    const filteredData = await getFilteredData(req);
-    const categories = await Product.distinct('category',)
+app.get('/c/', querySchema(), paginatedResults(), async (req, res) => {
+    const filteredData = res.results.results;
+    const categories = await Product.distinct('category')
     res.render('index.ejs', { products:filteredData,  categories});
 })
 
@@ -40,21 +41,50 @@ app.get('/data/:id', async (req, res) => {
     res.json(product);
 })
 
-app.get('/api/', async (req, res, next) => {
-    const filteredData = await getFilteredData(req);
-    res.json(filteredData);
+app.get('/api/', querySchema(), paginatedResults(), (req, res) => {
+    res.json(res.results);
 })
 
-const getFilteredData = async (req, res, next) => {
-    let querySchema = {};
-    const { category,pmin,pmax,s } = req.query
-    if(category) querySchema.category = {$in: category.split(',')}
-    if(pmin && pmax) querySchema.price = { $gte: parseInt(pmin), $lte: parseInt(pmax)};
-    else if(pmin) querySchema.price = { $gte: parseInt(pmin)}
-    else if(pmax) querySchema.price = { $lte: parseInt(pmax)}
-    if(s) querySchema.title = s.split('-').join(' ');
-    const filteredData = await Product.find(querySchema);
-    return filteredData;
+function querySchema() {
+    return async (req, res, next) => {
+        let querySchema = {};
+        const { category,pmin, pmax, s, p} = req.query;
+        if(category) querySchema.category = {$in: category.split(',')}
+
+        if(pmin && pmax) querySchema.price = { $gte: parseInt(pmin), $lte: parseInt(pmax)};
+        else if(pmin) querySchema.price = { $gte: parseInt(pmin)}
+        else if(pmax) querySchema.price = { $lte: parseInt(pmax)}
+
+        if(s) querySchema.title = s.split('-').join(' ');
+
+        res.querySchema = querySchema;
+        next()
+    }
+}
+
+function paginatedResults() {
+    return async (req, res, next) => {
+        const page  = parseInt(req.query.p) || 1;
+        const limit = 10;
+        const skip = (page - 1) * limit
+        results = {};
+
+        const length = await Product.collection.countDocuments(res.querySchema);
+        if( page != 0 ) {
+            results.prev = page - 1;
+        }
+        if( length > page * limit) {
+            results.next = page + 1;
+        }
+
+        try {
+            results.results = await Product.find(res.querySchema).limit(limit).skip(skip);
+            res.results = results;
+            next();
+        } catch(e) {
+            res.status(500).json({ message: e.message })
+        }
+    }
 }
 
 
--- public/js/filter-home.js
+++ public/js/filter-home.js
@@ -11,13 +11,14 @@ const filterBtn = document.querySelector('.filter-btn');
 const categoryFilter = new Filter('category');
 const minPriceFilter = new Filter('pmin')
 const maxPriceFilter = new Filter('pmax')
+const pageFilter = new Filter('p')
 
 filterItemEls.forEach(filter => {
     filter.addEventListener('click', (ev) => {
         categoryFilter.updateQuery(ev.target.value);
-        const updatedUrl = categoryFilter.getQueryString(',')
+        let updatedUrl = categoryFilter.getQueryString(',')
+        updatedUrl = clearPagination(updatedUrl);
         window.history.pushState({}, '',updatedUrl)
-        createFilteredEl();
         displayFilteredCategory()
     })
 })
@@ -30,50 +31,18 @@ filterBtn.addEventListener('click', (ev) => {
         minPriceFilter.reset();
         minPriceFilter.updateQuery(minPrice.value)
         minUpdateUrl = minPriceFilter.getQueryString(',')
+        minUpdateUrl = clearPagination(minUpdateUrl);
         window.history.pushState({}, '',minUpdateUrl)
     }
     if(maxPrice.value !== '') {
         maxPriceFilter.reset();
         maxPriceFilter.updateQuery(maxPrice.value)
         maxUpdateUrl = maxPriceFilter.getQueryString(',')
+        maxUpdateUrl = clearPagination(maxUpdateUrl);
         window.history.pushState({}, '',maxUpdateUrl)   
     }
-    createFilteredEl();
     displayFilteredCategory();
 })
-
-const displayFilteredCategory = async () => {
-    const filteredEl = await createFilteredEl();
-    while(productsEl.children.length > 0) await productsEl.removeChild(productsEl.firstChild);
-    productsEl.innerHTML = filteredEl;
-}
-
-const createFilteredEl = async () => {
-    const path = window.location.pathname;
-    const url = window.location.href
-    const filterUrl = path === '/c/' ? url.replace('/c/', '/api/') : url + 'api';
-    const res = await fetch(filterUrl);
-    const filteredData = await res.json();
-    const filteredEl = await filteredData.map((product) => {
-        return `<div class="product scale" data-id="${product._id}">
-        <a href="/products/${ product._id }"><img src="/${ product.image }" alt="" class="product-image"></a>
-                    <a href="/products/${ product.id } %>"><h4 class="product-header">${ product.title }</h4></a>
-            <i class="fa fa-star checked"></i>
-            <i class="fa fa-star checked"></i>
-            <i class="fa fa-star checked"></i>
-            <i class="fa fa-star checked"></i>
-            <i class="far fa-star "></i>
-            <p class="product-price">
-                ${product.price}$
-            </p>
-            <div class="row align-center" id="prodQuantity">
-                <button class="btn btn-primary" id="addToCart">Add to Cart</button>
-                <i class="far fa-heart" id="addWish" data-status="0"></i>
-            </div>
-        </div>`
-    })
-    return filteredEl.join('');
-}
 
 minPrice.addEventListener('input', (ev) => {
     if(minPrice.value || maxPrice.value) filterBtn.classList.remove('disabled');
@@ -92,7 +61,68 @@ filterResetBtnEl.addEventListener('click', (ev) => {
     categoryFilter.reset();
     minPriceFilter.reset();
     maxPriceFilter.reset();
+    pageFilter.reset();
     window.history.pushState({}, '','/');
-    createFilteredEl();
-    displayFilteredCategory()
+    displayFilteredCategory();
 })
+
+const loadProdEl = document.querySelector('.load-more')
+pageFilter.next = 2;
+loadProdEl.addEventListener('click', async () => {
+    pageFilter.reset();
+    pageFilter.updateQuery(`${pageFilter.next}`);
+    const updatedUrl = pageFilter.getQueryString(',');
+    if(pageFilter.next) {
+        window.history.pushState({}, '',updatedUrl);
+        loadMoreProduct();
+    }
+})
+
+//Display created html element on product section
+const displayFilteredCategory = async () => {
+    const filteredEl = await createHtmlElement();
+    productsEl.innerHTML = filteredEl.join('');
+}
+
+const loadMoreProduct = async () => {
+    if(pageFilter.next) {
+        const filteredEl = await createHtmlElement();
+        productsEl.insertAdjacentHTML('beforeend', filteredEl.join(''));
+    }
+}
+
+//Removes page query string in url
+const clearPagination = (query) => {
+    pageFilter.reset();
+    const re = /&?p=\d+&?/
+    return query.replace(re, '');
+}
+
+//Get data from database with query string and create html element
+const createHtmlElement = async () => {
+    const path = window.location.pathname;
+    const url = window.location.href
+    const filterUrl = path === '/c/' ? url.replace('/c/', '/api/') : url + 'api';
+    const res = await fetch(filterUrl);
+    const filteredData = await res.json();
+    pageFilter.next = filteredData.next
+    const filteredEl = await filteredData.results.map((product) => {
+        return `<div class="product scale" data-id="${product._id}">
+            <a href="/products/${ product._id }"><img src="/${ product.image }" alt="" class="product-image"></a>
+            <a href="/products/${ product.id } %>"><h4 class="product-header">${ product.title }</h4></a>
+            <i class="fa fa-star checked"></i>
+            <i class="fa fa-star checked"></i>
+            <i class="fa fa-star checked"></i>
+            <i class="fa fa-star checked"></i>
+            <i class="far fa-star "></i>
+            <p class="product-price">
+                ${product.price}$
+            </p>
+            <div class="row align-center" id="prodQuantity">
+                <button class="btn btn-primary" id="addToCart">Add to Cart</button>
+                <i class="far fa-heart" id="addWish" data-status="0"></i>
+            </div>
+        </div>`
+    })
+    return filteredEl;
+    }
